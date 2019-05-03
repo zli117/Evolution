6c60b5e3f4b19043120adf1c2d8d3af7a6b1d737
Zonglin Li <development.my6565@gmail.com>
2019-05-02 20:26:01 -0500

    Fixing flaky test

--- test/mutation_strategy_test.py
+++ test/mutation_strategy_test.py
@@ -35,17 +35,18 @@ def test_mutation_one_layer_fail():
 
     edge = Layer2()
 
-    assert not mutate.mutate(edge)
+    assert not mutate(edge)
 
 
-def test_mutation_one_layer_success():
+def test_mutation_success():
     mutate = MutateOneLayer()
 
     edge = Layer2()
     layer3 = TrackMutation(available_operations=(edge,))
     layer4 = TrackMutation(available_operations=(layer3,))
 
-    assert mutate.mutate(layer4)
+    layer4.mutation_add_vertex()
 
-    # XOR
-    assert layer3.mutated != layer4.mutated
+    layer4.invalidate_layer_count()
+
+    assert mutate(layer4)
