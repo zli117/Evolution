534ba3deea10b1ce083419ddd3564aeba35f1688
Zonglin Li <development.my6565@gmail.com>
2019-05-10 22:18:58 -0500

    Test

--- evolution/train/model.py
+++ evolution/train/model.py
@@ -23,8 +23,6 @@ class BaseTrainer(ABC):
     def optimizer_factory(self) -> keras.optimizers.Optimizer:
         pass
 
-def test(param):
-    return 1
 
 @dataclass
 class Trainer(BaseTrainer):
@@ -79,8 +77,11 @@ class Trainer(BaseTrainer):
 
         with Pool(self.num_process) as pool:
             # history = list(pool.map(worker, self._data_generator()))
-            history = list(pool.map(test, self._data_generator()))
+            history = list(pool.map(self.test, self._data_generator()))
             return float(np.mean(history))
+
+    def test(self, param):
+        return 1
 
     def optimizer_factory(self) -> keras.optimizers.Optimizer:
         return keras.optimizers.Adam()
